509a10dd4310cf4e602fda13d5aa048a94ee07dd
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-12-13 15:45:23 -0700

    Create index.js file + test basic axios get request

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, function (){
+    console.log("Server started. Listening on port 3000.");
+});
+
+app.get("/", (req, res) => {
+    res.send("hello");
+   });
+   
