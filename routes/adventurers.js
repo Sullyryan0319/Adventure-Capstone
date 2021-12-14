9d99d0d07b36b85c803495e72df0a9fa5e659bf9
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-12-13 22:14:12 -0700

    Add post, get and get by id routes for adventurer

--- routes/adventurers.js
+++ routes/adventurers.js
@@ -1,24 +1,46 @@
-const Adventurer = require('../models/adventurer');
-const express = require('express');
+const { Adventurer, validate } = require("../models/adventurer");
+const express = require("express");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try {
-
-        const adventurer = new Adventurer({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName
-
-        });
-
-        await adventurer.save();
-
-        return res.send(adventurer);
-
-    } catch (ex) {
-        return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const adventurers = await Adventurer.find();
+    return res.send(adventurers);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+ 
+  const adventurer = await Adventurer.findById(req.params.id);
+  if (!adventurer)
+  return res.status(400).send(`The adventurer with id "${req.params.id}" d
+ oes not exist.`);
+  return res.send(adventurer);
+  } catch (ex) {
+  return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+ });
+ 
+
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error);
+
+    const adventurer = new Adventurer({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+
+    await adventurer.save();
+
+    return res.send(adventurer);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 
 module.exports = router;
