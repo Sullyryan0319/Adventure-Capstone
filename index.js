752f1252c3154e3aea5a9f149efcdbf3c491cd88
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-12-13 17:31:38 -0700

    Add config folder and default json

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require('mongoose');
+
+mongoose
+.connect('mongodb+srv://<username>:<password>@<clustername>.6si6q.mongodb.net/<dbname>?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => console.log('Connected to MongoDB...'))
+ .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
+
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(3000, function (){
     console.log("Server started. Listening on port 3000.");
